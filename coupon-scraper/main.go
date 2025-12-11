bb91514f9d9c69c0f8f9ffc38971408c494d1601
DavidJ <davidjunqueira@outlook.com>
2025-12-11 18:24:00 +0100

    fmt: fixed bad formatting

--- coupon-scraper/main.go
+++ coupon-scraper/main.go
@@ -13,96 +13,86 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
-
-
-
-func StringSetToSlice (set map[string]struct{}) ([]string){
+func StringSetToSlice(set map[string]struct{}) []string {
 	resultSlice := make([]string, len(set))
-	for key := range set{
+	for key := range set {
 		resultSlice = append(resultSlice, key)
 	}
 	return resultSlice
 }
 
-
-func main(){
+func main() {
 	godotenv.Load("../.env")
 	gamesCodesURL := os.Getenv("GAMECODES_URL")
 	redemptionServiceURL := os.Getenv("REDEMPTION_SERVICE_URL")
 	addTicketsEndpoint := redemptionServiceURL + "/v1/coupons"
 	codes, err := ScrapCodesFromURL(gamesCodesURL)
-	if err != nil{
+	if err != nil {
 		log.Fatalf("Couldn't scrap HTML from URL, got error: %v", err.Error())
 	}
 	// TODO inject actual backend url
-	err = SendCurrentCoupons(addTicketsEndpoint,codes)
-	if err != nil{
+	err = SendCurrentCoupons(addTicketsEndpoint, codes)
+	if err != nil {
 		log.Printf("Got error when trying to send coupon codes: %v", err.Error())
-	}	
+	}
 }
 
-
-
 func ScrapCodesFromURL(url string) (map[string](struct{}), error) {
-	req, err := http.NewRequest("GET",url,nil)
-	codes := make(map[string](struct{}),10)
-	if err != nil{
+	req, err := http.NewRequest("GET", url, nil)
+	codes := make(map[string](struct{}), 10)
+	if err != nil {
 		return map[string](struct{}){}, err
 	}
 
 	res, err := http.DefaultClient.Do(req)
-	if err != nil{
+	if err != nil {
 		return map[string](struct{}){}, err
 	}
 
 	defer res.Body.Close()
-	if err != nil{
-		return map[string](struct{}){}, err
-	}	
 	err = ExtractCodesFromBody(res.Body, &codes)
-	if err != nil{
+	if err != nil {
 		return map[string](struct{}){}, err
 	}
 	return codes, nil
 }
 
-func ExtractCodesFromBody(body io.Reader, codes *map[string](struct{})) (error){
+func ExtractCodesFromBody(body io.Reader, codes *map[string](struct{})) error {
 	doc, err := goquery.NewDocumentFromReader(body)
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 	doc.Find("body").Find("td").Find("a").Each(func(i int, s *goquery.Selection) {
 		elem, exists := s.Attr("data-gamecode")
-		if exists{
+		if exists {
 			(*codes)[elem] = struct{}{}
-		}	
+		}
 	})
 	fmt.Println(*codes)
 	return nil
 }
 
-func SendCurrentCoupons(url string, extractedCoupons map[string](struct{})) (error){
-	type bodyParams struct{
+func SendCurrentCoupons(url string, extractedCoupons map[string](struct{})) error {
+	type bodyParams struct {
 		Codes []string `json:"coupon_codes"`
 	}
 	payloadData := bodyParams{Codes: StringSetToSlice(extractedCoupons)}
-	log.Println("Trying to send the following fetched codes: ",payloadData)
+	log.Println("Trying to send the following fetched codes: ", payloadData)
 	marshalledPayload, err := json.Marshal(payloadData)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 
-	req, err := http.NewRequest("POST",url, bytes.NewBuffer(marshalledPayload))
-	if err != nil{
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(marshalledPayload))
+	if err != nil {
 		return err
 	}
-	
+
 	resp, err := http.DefaultClient.Do(req)
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	if resp.StatusCode != http.StatusCreated{
+	if resp.StatusCode != http.StatusCreated {
 		return fmt.Errorf("post gamecodes did not result in 200 http code, got code: %w", resp.StatusCode)
 	}
 	return nil
